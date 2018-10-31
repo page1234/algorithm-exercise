e32bdd353fa13289cef366355ee90283310792c8
page <baiyejianxin@126.com>
2018-10-31 08:22:19 +0800

    [Add] Add get maximum method and merge method.

--- src/test/java/com/page/algorithm/ExerciseTest.java
+++ src/test/java/com/page/algorithm/ExerciseTest.java
@@ -1,10 +1,12 @@
 package com.page.algorithm;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ExerciseTest {
 
@@ -14,7 +16,7 @@ public class ExerciseTest {
 
         float median = MedianExercise.median(list);
 
-        Assert.assertEquals(2.5, median, 0.0);
+        assertEquals(2.5, median, 0.0);
     }
 
     @Test
@@ -23,6 +25,56 @@ public class ExerciseTest {
 
         float median = MedianExercise.median(list);
 
-        Assert.assertEquals(2, median, 0);
+        assertEquals(2, median, 0);
+    }
+
+    @Test
+    public void shouldReturnTheMaximumValueBetweenTwoArrayList() {
+        List<Integer> firstList = Arrays.asList(1, 3);
+        List<Integer> secondList = Arrays.asList(2, 4);
+
+        int max = MedianExercise.getMaximum(firstList, secondList);
+
+        assertEquals(4, max);
+    }
+
+    @Test
+    public void shouldMergeTwoListIntoOne() {
+        List<Integer> firstList = Arrays.asList(1, 3);
+        List<Integer> secondList = Arrays.asList(2, 4);
+
+        List<Integer> mergedList = MedianExercise.merge(firstList, secondList);
+
+        assertEquals(Arrays.asList(1, 2, 3, 4), mergedList);
+    }
+
+    @Test
+    public void shouldMergeTwoListIntoOneWhenFistListIsEmpty() {
+        List<Integer> firstList = Collections.emptyList();
+        List<Integer> secondList = Arrays.asList(2, 4);
+
+        List<Integer> mergedList = MedianExercise.merge(firstList, secondList);
+
+        assertEquals(Arrays.asList(2, 4), mergedList);
+    }
+
+    @Test
+    public void shouldMergeTwoListIntoOneWhenSecondListIsEmpty() {
+        List<Integer> firstList = Arrays.asList(2, 4);
+        List<Integer> secondList = Collections.emptyList();
+
+        List<Integer> mergedList = MedianExercise.merge(firstList, secondList);
+
+        assertEquals(Arrays.asList(2, 4), mergedList);
+    }
+
+    @Test
+    public void shouldMergeTwoListIntoOneWhenHaveSameItemInThoseList() {
+        List<Integer> firstList = Arrays.asList(1, 2);
+        List<Integer> secondList = Arrays.asList(1, 2);
+
+        List<Integer> mergedList = MedianExercise.merge(firstList, secondList);
+
+        assertEquals(Arrays.asList(1, 1, 2, 2), mergedList);
     }
 }
--- src/test/java/com/page/algorithm/MedianExercise.java
+++ src/test/java/com/page/algorithm/MedianExercise.java
@@ -1,6 +1,7 @@
 package com.page.algorithm;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MedianExercise {
@@ -12,5 +13,41 @@ public class MedianExercise {
                 new BigDecimal(list.get(size / 2 - 1) + list.get(size / 2))
                         .divide(new BigDecimal(2), 1, BigDecimal.ROUND_CEILING)
                         .floatValue();
+    }
+
+
+    public static int getMaximum(List<Integer> firstList, List<Integer> secondList) {
+        int maxInFirstList = firstList.get(firstList.size() - 1);
+        int maxInSecondList = secondList.get(secondList.size() - 1);
+        return maxInFirstList > maxInSecondList ? maxInFirstList : maxInSecondList;
+    }
+
+
+    public static List<Integer> merge(List<Integer> firstList, List<Integer> secondList) {
+        int secondListIndex = 0;
+        int secondListSize = secondList.size();
+
+        List<Integer> mergedList = new ArrayList<Integer>();
+        for (int index = 0, size = firstList.size(); index < size; index++) {
+            int valueInFirstList = firstList.get(index);
+            if (secondListIndex >= secondListSize) {
+                mergedList.add(valueInFirstList);
+            } else {
+                int valueInSecondList = secondList.get(secondListIndex);
+                if (valueInFirstList < valueInSecondList) {
+                    mergedList.add(valueInFirstList);
+                } else {
+                    mergedList.add(valueInSecondList);
+                    secondListIndex++;
+                    index--;
+                }
+            }
+        }
+
+        for (int index = secondListIndex; index < secondListSize; index++) {
+            mergedList.add(secondList.get(index));
+        }
+
+        return mergedList;
     }
 }
